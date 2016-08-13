dd0f9c1b806f2f152f5b67ec2a727a0ea217ee92
kimi <mengjiechen1989@icloud.com>
2016-08-14 00:35:41 +0800

    修复排序BUG

--- 网易新闻/网易新闻/Classes/Home/Model/WYChannelModel.m
+++ 网易新闻/网易新闻/Classes/Home/Model/WYChannelModel.m
@@ -14,14 +14,18 @@
 {
     //1.JSON路径
     NSString *filePath = [[NSBundle mainBundle]pathForResource:@"topic_news.json" ofType:nil];
+    
     //2.获取临时字典(JSON需要先用NSData接收)
     NSData *data = [NSData dataWithContentsOfFile:filePath];
+    
     //反序列化
     NSDictionary *tempDict = [NSJSONSerialization JSONObjectWithData:data options:0 error:NULL];
+    
     //3.字典转模型,利用yymodel
     NSArray *result = [NSArray yy_modelArrayWithClass:[self class] json:tempDict[@"tList"]];
+    
     //4.对包含模型的数组进行排序
-    [result sortedArrayUsingComparator:^NSComparisonResult(WYChannelModel * _Nonnull obj1, WYChannelModel *  _Nonnull obj2) {
+     result = [result sortedArrayUsingComparator:^NSComparisonResult(WYChannelModel * _Nonnull obj1, WYChannelModel *  _Nonnull obj2) {
         return [obj1.tid compare:obj2.tid];
     }];
     return result;
